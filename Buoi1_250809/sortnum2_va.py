b0e337e068e9ef252d09a2c148c4c8ac9bc21e6b
toanpham31231023715-cyber <toanpham.31231023715@st.ueh.edu.vn>
2025-11-04 20:44:22 +0700

    Create sortnum2_va.py

---
+++ Buoi1_250809/sortnum2_va.py
@@ -0,0 +1,11 @@
+num1 = float(input("Num 1: "))
+num2 = float(input("Num 2: "))
+
+if num1 < num2:
+    final1 = num1
+    final2 = num2
+else:
+    final1 = num2
+    final2 = num1
+
+print(final1, final2)
